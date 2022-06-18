7eacdb52539e0a508420267c20d652980dd1b727
Cheel <44915367+miche715@users.noreply.github.com>
2022-06-18 11:48:18 +0900

    2020.06.18

---
+++ python/p_68644.py
@@ -0,0 +1,16 @@
+# https://programmers.co.kr/learn/courses/30/lessons/68644
+# 두 개 뽑아서 더하기
+
+import itertools
+
+def solution(numbers):
+    answer = set()
+    com = tuple(itertools.combinations(numbers, 2))
+    
+    for c in com:
+        answer.add(c[0] + c[1])
+    
+
+    return sorted(list(answer))
+
+print(solution([5, 0, 2, 7]))
