2e64feb5dac201634e0a23a4b380fbcdd5d4e95d
Cheel <44915367+miche715@users.noreply.github.com>
2022-06-25 10:32:56 +0900

    2022.06.25

--- python/p_12922.py
+++ python/p_12922.py
@@ -1,3 +1,6 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12922
+# 수박수박수박수박수박수?
+
 def solution(n):
     answer = ""
     watermelon = ["수", "박"]
---
+++ python/p_12925.py
@@ -0,0 +1,5 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12925
+# 문자열을 정수로 바꾸기
+
+def solution(s):
+    return eval(s)
