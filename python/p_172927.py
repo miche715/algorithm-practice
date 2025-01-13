5b0cfdd1e7957186415cc46a774119ddd56fb3db
Cheel <44915367+miche715@users.noreply.github.com>
2025-01-13 12:21:05 +0900

    2025.01.13

--- python/p_172927.py
+++ python/p_172927.py
@@ -3,25 +3,37 @@
 
 def solution(picks, minerals):
     answer = 0
-    stemina_dict = {"diamond": 2, "iron": 1, "stone": 0}
-    stemina_rate = {}
+    stemina_diamond_pick = {"diamond": 1, "iron": 1, "stone": 1}
+    stemina_iron_pick = {"diamond": 5, "iron": 1, "stone": 1}
+    stemina_stone_pick = {"diamond": 25, "iron": 5, "stone": 1}
+    stemina_ranking = {}
     
+    if (picks[0] + picks[1] + picks[2]) * 5 < len(minerals):
+         minerals = minerals[:(picks[0] + picks[1] + picks[2]) * 5]
+
     stemina_sum = 0
     stemina_key = []
     for i, mineral in enumerate(minerals):
-        stemina_sum = stemina_sum + stemina_dict[mineral]
+        stemina_sum = stemina_sum + stemina_stone_pick[mineral]
         stemina_key.append(i)
         if i % 5 == 4:
-            stemina_rate[tuple(stemina_key)] = stemina_sum
+            stemina_ranking[tuple(stemina_key)] = stemina_sum
             stemina_sum = 0
             stemina_key.clear()
     if stemina_sum > 0:
-        stemina_rate[tuple(stemina_key)] = stemina_sum
+        stemina_ranking[tuple(stemina_key)] = stemina_sum
     
-    
-    # 아직 푸는 중...      
-    
+    for sr in sorted(stemina_ranking.items(), key=lambda x: x[1], reverse=True):
+        for i in sr[0]:
+            if picks[0] > 0:
+                pick = 0
+                answer = answer + stemina_diamond_pick[minerals[i]]
+            elif picks[1] > 0:
+                pick = 1
+                answer = answer + stemina_iron_pick[minerals[i]]
+            elif picks[2] > 0:
+                pick = 2
+                answer = answer + stemina_stone_pick[minerals[i]]
+        picks[pick] = picks[pick] - 1
 
     return answer
-
-solution([1, 3, 2], ["diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"])
