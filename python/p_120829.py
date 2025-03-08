db53ec51eb1f9ca14964dec10093af1a81ed8bc9
Cheel <44915367+miche715@users.noreply.github.com>
2025-03-08 20:46:58 +0900

    2025.03.08

---
+++ python/p_120829.py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120829
+# 각도기
+
+def solution(angle):
+    if 0 < angle < 90:
+        return 1
+    elif angle == 90:
+        return 2
+    elif 90 < angle < 180:
+        return 3
+    elif angle == 180:
+        return 4
