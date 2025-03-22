8debe78562cd488d76b206f2692a7aac16601316
Cheel <44915367+miche715@users.noreply.github.com>
2025-03-22 21:06:18 +0900

    2025.03.21

---
+++ sql/s_59037.sql
@@ -0,0 +1,11 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59037
+-- 어린 동물 찾기
+
+SELECT
+    ANIMAL_ID, NAME
+FROM
+    ANIMAL_INS
+WHERE
+    INTAKE_CONDITION != 'Aged'
+ORDER BY
+    ANIMAL_ID ASC
