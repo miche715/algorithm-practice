9660a4e62ddbb18596cf20a4249e4c24076b15f5
이창희 <44915367+miche715@users.noreply.github.com>
2025-11-28 14:52:10 +0900

    2025.11.28

--- python/p_340207.py
+++ python/p_340207.py
@@ -1,3 +1,6 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/340207
+# 문자 출력
+
 message = "Let's go!"
 
 print("3\n2\n1\n")
---
+++ python/p_451808.py
@@ -0,0 +1,45 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/451808
+# 숫자 야구
+
+from itertools import permutations
+
+def make_strike_ball_list(result):
+    return [int(r[0]) for r in result.split()]
+
+
+def compare_strike_ball(a, b):
+    a = str(a)
+    b = str(b)
+
+    strike = sum(a[i] == b[i] for i in range(4))
+    ball = sum((digit in b) for digit in a) - strike
+
+    return [strike, ball]
+
+
+def filter_candinate(candidate, number, result):
+    new_candinate = []
+    for c in candidate:
+        if compare_strike_ball(c, number) == result:
+            new_candinate.append(c)
+
+    return new_candinate
+
+
+def solution(n, submit):
+    answer = 1234
+    candinate = [int(''.join(map(str, p))) for p in permutations(range(1, 10), 4)]
+
+    for i in range(0, n):
+        result = make_strike_ball_list(submit(answer))
+        if result == [4, 0]: 
+            return answer
+
+        candinate = filter_candinate(candinate, answer, result)
+        
+        # 여기서 이제 가장 최적의 다음 시도값(answer)을 찾으면 됨.
+        answer = candinate[0]
+
+    return answer
+
+
